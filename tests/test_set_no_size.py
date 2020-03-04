eeee9bf4286f1a1ee88c0c21936e851c43208fd9
Daniel Garnier-Moiroux <git@garnier.wf>
2020-03-04 08:31:31 +0100

    add a test for the set without "size"

---
+++ tests/test_set_no_size.py
@@ -0,0 +1,24 @@
+#####################
+# "real" code"      #
+#####################
+class Set:
+    def __init__(self):
+        self._empty = True
+
+    def empty(self):
+        return self._empty
+
+    def add(self, element):
+        self._empty = False
+
+#####################
+# Here be our tests #
+#####################
+def test_emptiness():
+    empty = Set()
+    one = Set()
+
+    one.add("1")
+
+    assert empty.empty() is True
+    assert one.empty() is False
