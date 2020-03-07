57b082f31f2fcbab4500449efe5f6038f96bd44e
Daniel Garnier-Moiroux <git@garnier.wf>
2020-03-07 21:38:05 +0100

    simplify "example" test

--- python/tests/test_example.py
+++ python/tests/test_example.py
@@ -7,12 +7,6 @@ def increment(value):
 #####################
 # Here be our tests #
 #####################
-import pytest
-
 def test_increment():
     assert increment(5) == 6
     assert increment(-5) == -4
-
-def test_wrong_type():
-    with pytest.raises(TypeError):
-        increment()
