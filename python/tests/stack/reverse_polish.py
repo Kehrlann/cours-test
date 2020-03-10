b6538a07d32554670fcc2b2d49ad946ed372eea4
Daniel Garnier-Moiroux <git@garnier.wf>
2020-03-10 22:18:29 +0100

    sample reverse polish, with tests

---
+++ python/tests/stack/reverse_polish.py
@@ -0,0 +1,21 @@
+from tests.stack.stack import Stack
+
+
+def rpn(*args):
+    stack = Stack()
+    for arg in args:
+        if arg == "+":
+            one = stack.pop()
+            two = stack.pop()
+            stack.push(one + two)
+        elif arg == "*":
+            one = stack.pop()
+            two = stack.pop()
+            stack.push(one * two)
+        elif arg == "-":
+            one = stack.pop()
+            two = stack.pop()
+            stack.push(two - one)
+        else:
+            stack.push(arg)
+    return stack.pop()
---
+++ python/tests/stack/test_reverse_polish.py
@@ -0,0 +1,7 @@
+from tests.stack.reverse_polish import rpn
+
+
+def test_reverse_polish():
+    assert rpn(1, 2, 3, "+", "+") == 6
+    assert rpn(1) == 1
+    assert rpn(3, 4, "*", 2, "-") == 10
