c7683928c14a16738dfa4b67b89f1bce08d15a84
Daniel Garnier-Moiroux <git@garnier.wf>
2020-03-11 20:48:10 +0100

    bootstrap presentation

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+git submodule update --init
+rm reveal.js/index.html
+ln -s "$SCRIPT_DIR"/index.html "$SCRIPT_DIR"/reveal.js/index.html
+ln -s "$SCRIPT_DIR"/images "$SCRIPT_DIR"/reveal.js/images
