f14a15841bb70a082ebd1aacd619bd29e47f5d90
Daniel Garnier-Moiroux <git@garnier.wf>
2020-03-12 22:17:58 +0100

    minesweeper skeletton

---
+++ python/tests/minesweeper/minesweeper.py
@@ -0,0 +1,21 @@
+class Minesweeper:
+    def reveal(self, x, y):
+        pass
+
+    def is_finished(self):
+        pass
+
+    def current_state(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = Minesweeper()
+
+    while not game.is_finished():
+        print_board(game.current_state())
+        print_help_message()
+        x, y = read_input()
+        game.reveal(x, y)
+
+    game.print_result()
