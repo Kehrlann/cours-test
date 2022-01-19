1822d6755648b3fc63484504ba207756e717520f
Daniel Garnier-Moiroux <git@garnier.wf>
2022-01-19 11:27:22 +0100

    Update revealjs

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 git submodule update --init
-rm reveal.js/index.html
+
+pushd reveal.js
+  git fetch
+  git checkout -- package.json index.html
+  git checkout origin/master
+  npm i
+popd
+
 ln -s "$SCRIPT_DIR"/index.html "$SCRIPT_DIR"/reveal.js/index.html
 ln -s "$SCRIPT_DIR"/images "$SCRIPT_DIR"/reveal.js/images
 
-pushd reveal.js
-  npm i
-popd
